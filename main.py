9fa3b1ce72d93e13c472ad08054c897f186c3315
Luke Petrolekas <luke.petrolekas@gmail.com>
2020-11-14 23:45:16 -0500

    fix dialogue bug, add fn_in/fn_out

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import re
+import os
+import sys
+
+DIALOGUE = re.compile(r'\\begin\{dialogue\}\{\\(?P<name>\w+)\/(\s\((?P<direction>.*)\))?\}\n\t?(?P<dialogue>.*)\n\\end\{dialogue\}')
 
 if __name__ == '__main__':
-    print("Hello Word")
+    fn_in = sys.argv[1]
+    fn_out = sys.argv[2]
