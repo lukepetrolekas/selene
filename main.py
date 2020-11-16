f2f0a5cadca696d3b3e44cfa58e9f734118fbe4a
Luke Petrolekas <luke.petrolekas@gmail.com>
2020-11-16 18:43:30 -0500

    add actor rules

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from commands import *
 ACTOR = re.compile(r"\\@ifdefinable\{\\(?P<alias>[\w\d]+)\}{\\def\\.+\/\{(?P<actor>.+)\}\}")
 DIALOGUE = re.compile(r'\\begin\{dialogue\}\{\\(?P<actor>\w+)\/(\s\((?P<blocking>.*)\))?\}\n\t?(?P<line>.*)\n\\end\{dialogue\}')
 
+def format_line(s):
+    return re.sub("--", u"\u2014", s) # em dash
+
 if __name__ == '__main__':
     fn_in = sys.argv[1]
     fn_out = sys.argv[2]
@@ -21,7 +24,7 @@ if __name__ == '__main__':
             command_builders.append(ActorBuilder(m.start(), m.group("alias"), m.group("actor")))
 
         for m in re.finditer(DIALOGUE, data):
-            command_builders.append(LineBuilder(m.start(), m.group("actor"), m.group("blocking"), m.group("line")))
+            command_builders.append(LineBuilder(m.start(), m.group("actor"), m.group("blocking"), format_line(m.group("line"))))
 
     sorted(command_builders, key=lambda x: x.cindex)
     [print(x) for x in command_builders]
