1e7f78aed529b0d9cf142ad31d38d43043c06657
Luke Petrolekas <luke.petrolekas@gmail.com>
2020-11-16 19:07:07 -0500

    resolve shorthands in dialogue

--- main.py
+++ main.py
@@ -16,15 +16,23 @@ if __name__ == '__main__':
     fn_out = sys.argv[2]
 
     command_builders = []
+    actor_resolver = {}
 
     with open(fn_in, 'r') as file:
         data = file.read()
 
         for m in re.finditer(ACTOR, data):
             command_builders.append(ActorBuilder(m.start(), m.group("alias"), m.group("actor")))
+            actor_resolver[m.group("alias")] = m.group("actor")
 
         for m in re.finditer(DIALOGUE, data):
             command_builders.append(LineBuilder(m.start(), m.group("actor"), m.group("blocking"), format_line(m.group("line"))))
+
+    # update all character shorthands
+    for b in command_builders:
+        if isinstance(b, commands.LineBuilder):
+            for k,v in actor_resolver.items():
+                b.line = re.sub(F"\\\\{k}\/", v, b.line)
 
     sorted(command_builders, key=lambda x: x.cindex)
     [print(x) for x in command_builders]
