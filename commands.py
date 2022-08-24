3da1a658bd825e8b16ed0ddaa852f16011c3868b
Luke Petrolekas <luke.petrolekas@gmail.com>
2022-08-24 19:13:23 -0400

    heavy changes for rewritten version

--- commands.py
+++ commands.py
@@ -1,3 +1,30 @@
+import re
+
+class DigiplayBuilder():
+    _shorthand = re.compile(r'\\(\w+)/')
+
+    def __init__(self, s):
+        self.s = s
+        self.actor_builders = ActorBuilder.extract(s)
+        self.command_builders = ["label digiplay:"] + LineBuilder.extract(s)
+
+    def format(self, s):
+        # very obvious optimization. If there is no \, there is nothing
+        # needed to check for substitutes.
+        if '\\' not in s:
+            return s
+
+        for ab in self.actor_builders:
+            s = s.replace(F"\\{ab.alias}/", ab.actor)
+
+        return s
+
+    def characters(self):
+        return "label characters:\n" + '\n'.join([str(x) for x in self.actor_builders])
+
+    def __str__(self):
+        return '\n'.join([self.format(str(x)) for x in self.command_builders])
+
 class CommandBuilder():
     def __init__(self, cindex):
         self.cindex = cindex
@@ -21,20 +48,70 @@ class SlugBuilder(CommandBuilder):
         return F"    scene bg {self.location}" + (F"\n    with {self.transition}" if self.transition else "")
 
 class ActorBuilder(CommandBuilder):
-    def __init__(self, cindex, alias, actor):
+    _actor = re.compile(r"\\@ifdefinable\{\\(?P<alias>[\w\d]+)\}{\\def\\.+\/\{(?P<actor>.+)\}\}")
+
+    @staticmethod
+    def extract(s, offset=0):
+        actors = []
+
+        for m in re.finditer(ActorBuilder._actor, s):
+            actors.append(ActorBuilder(offset + m.start(), m.group("alias"), m.group("actor"), "FFF"))
+
+        return actors
+
+    def __init__(self, cindex, alias, actor, colorHex):
         super().__init__(cindex)
         self.alias = alias
         self.actor = actor
+        self.colorHex = colorHex
 
     def __str__(self):
-        return F"define {self.alias} = Character(\"{self.actor.upper()}\", color=\"#fff\")"
+        return F"    define {self.alias} = Character(\"{self.actor.upper()}\", color=\"#{self.colorHex}\")"
 
 class LineBuilder(CommandBuilder):
-    def __init__(self, cindex, actor, blocking, line):
+    _regex = re.compile(r'\\begin{dialogue}{\\(?P<actor>\w+)/}\s*(\#(?P<expr>\w+))?\s*(\@(?P<block>\w+))?\n(?P<line>(.*|\n)+)\n\\end{dialogue}')
+    _italics = re.compile(r'\\emph\{([^\}]+)\}')
+
+    @staticmethod
+    def extract(s, offset=0):
+        lines = []
+
+        for m in re.finditer(LineBuilder._regex, s):
+            lines.append(LineBuilder(offset + m.start(), m.group("actor"), m.group("expr"), m.group("block"), m.group("line")))
+
+        return lines
+
+    @staticmethod
+    def format(s):
+        s = re.sub("--", u"\u2014", s) # em dash
+        s = re.sub("'", u"\u2019", s) # apostrophe to single quote (asthetic)
+        s = re.sub("\.\.\.", u"\u2026", s) # ellipsis
+        s = re.sub(f"\"", "\\\"", s) # quotes within line
+        s = LineBuilder._italics.sub(r'{i}\g<1>{/i}', s) #italics
+        s = s.strip()
+        return s
+
+    def __init__(self, cindex, actor, expr, block, line):
         super().__init__(cindex)
         self.actor = actor
-        self.blocking = blocking
+        self.expr = expr
+        self.block = block
         self.line = line
 
     def __str__(self):
-        return F"    {self.actor} \"{self.line}\""
+        output = ""
+        for index, l in enumerate(self.line.splitlines()):
+            l = LineBuilder.format(l)
+
+            match l:
+                case '\\beat':
+                    continue
+                case '\\pause':
+                    continue
+
+            if index == 0:
+                output = F"    {self.actor} \"{l}\""
+            else:
+                output = output + F"\n    extend \" {l}\""
+
+        return output
--- main.py
+++ main.py
@@ -7,24 +7,10 @@ from commands import *
 
 SLUG = re.compile(r'\\(ex|in)tslug\[(?P<time>.*)\]\{(?P<location>.*)\}(\s+\% WITH (?P<transition>.*))?')
 
-ACTOR = re.compile(r"\\@ifdefinable\{\\(?P<alias>[\w\d]+)\}{\\def\\.+\/\{(?P<actor>.+)\}\}")
-DIALOGUE = re.compile(r'\\begin\{dialogue\}\{\\(?P<actor>\w+)\/(\s\((?P<blocking>.*)\))?\}\n\t?(?P<line>.*)\n\\end\{dialogue\}')
-
 FADE_IN = re.compile(r'\\fadein')
 FADE_OUT = re.compile(r'\\fadeout')
 
 STRIP_PAREN = re.compile(r'\\paren\{[^\}]+\}') # temp
-
-def format_line(s):
-    s = re.sub("--", u"\u2014", s) # em dash
-    s = re.sub(f"\"", "\\\"", s) # quotes
-    s = re.sub(STRIP_PAREN, '', s) # temp
-    s = re.sub("'", u"\u2019", s) # apostrophe to single quote
-    s = re.sub("\.\.\.", u"\u2026", s) # ellipsis
-
-    # clean up accidental tabs in LaTeX
-    s = s.strip()
-    return s
 
 if __name__ == '__main__':
     fn_in = sys.argv[1]
@@ -33,38 +19,13 @@ if __name__ == '__main__':
     command_builders = []
     actor_resolver = {}
 
-    with open(fn_in, 'r') as file:
+    with open(fn_in, 'r', encoding='utf-8') as file:
         data = file.read()
 
-        for m in re.finditer(ACTOR, data):
-            command_builders.append(ActorBuilder(m.start(), m.group("alias"), m.group("actor")))
-            actor_resolver[m.group("alias")] = m.group("actor")
-            
-        for m in re.finditer(FADE_IN, data):
-            command_builders.append(CodeBuilder(m.start(), "label start:"))
+        d = DigiplayBuilder(data)
 
-        for m in re.finditer(SLUG, data):
-            command_builders.append(SlugBuilder(m.start(), m.group("time"), m.group("location"), m.group("transition")))
+        with open(fn_out + "characters.rpy", 'w') as outscr:
+            outscr.write(d.characters())
 
-        for m in re.finditer(DIALOGUE, data):
-            command_builders.append(LineBuilder(m.start(), m.group("actor"), m.group("blocking"), format_line(m.group("line"))))
-
-        for m in re.finditer(FADE_OUT, data):
-            command_builders.append(CodeBuilder(m.start(), "    return\n"))
-
-    # update all scenes
-    for b in command_builders:
-        if isinstance(b, commands.SlugBuilder):
-            b.location = re.sub("[^A-Za-z]", '', b.location) # temp
-                
-    # update all character shorthands
-    for b in command_builders:
-        if isinstance(b, commands.LineBuilder):
-            for k,v in actor_resolver.items():
-                b.line = re.sub(F"\\\\{k}\/", v, b.line)
-
-    command_builders.sort(key=lambda x: x.cindex)
-    r = '\n'.join([x.__str__() for x in command_builders])
-
-    with open(fn_out, 'w') as outscr:
-        outscr.write(r)
+        with open(fn_out + "digiplay.rpy", 'w') as outscr:
+            outscr.write(str(d))
